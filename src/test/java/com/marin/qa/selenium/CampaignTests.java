ac26c6db5dd2fd0071b7a06f511a1488ed7c0a90
Manish Madhusoodan <mmadhusoodan@marinsoftware.com>
2014-08-12 19:12:21 -0700

    Mavenized the structure

---
+++ src/test/java/com/marin/qa/selenium/CampaignTests.java
@@ -0,0 +1,13 @@
+package com.marin.qa.selenium;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.marin.qa.selenium.campaigns.TestCampaigns1;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestCampaigns1.class })
+public class CampaignTests {
+
+}   
